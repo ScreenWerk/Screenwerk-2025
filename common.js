325d1460114f446dda8c214321f6b41cc61672dd
mitselek <mihkel.putrinsh@gmail.com>
2024-12-20 12:20:51 +0200

    refactor json fetching

--- common.js
+++ common.js
@@ -1,4 +1,16 @@
 function toDateTimeString(ISODate) {
   return ISODate.slice(0, 10) + ' ' + ISODate.slice(11, 19)
 }
- 
+
+async function fetchJSON(url) {
+  const r = await fetch(url)
+  try {
+    if (r.status !== 200) {
+      return false
+    }
+    return await r.json()
+  } catch (e) {
+    console.error(`Error fetching ${url}: ${e}`)
+    return false
+  }
+}
