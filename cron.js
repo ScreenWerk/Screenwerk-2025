e659e036a483c4cd9915a233f16fffe73777c1fa
mitselek <mihkel.putrinsh@gmail.com>
2025-01-07 13:25:30 +0200

    describe cron functionality

--- cron.js
+++ cron.js
@@ -1,19 +1,30 @@
 // Disclaimer: no semicolons, if unnecessary, are used in this project
+//
+// Example input:
+// const cron = new Cron([
+//     { cronExpression: '*/5 * * * * *', schedule_id: '55adf9d94ecca5c17a5994db' },
+//     ...
+// ])
+
+
 
 class Cron {
-  constructor() {
-    this.schedules = [];
-  }
+    constructor(schedules) {
+        this.schedules = schedules
+    }
 
-  addSchedule(cronExpression, task) {
-    this.schedules.push({ cronExpression, task });
-  }
+    // Calculate and return the previous schedule executed
+    // Decuide based on the current time and the cron expressions in the schedules
+    prev() {
+    }
 
-  removeSchedule(task) {
-    this.schedules = this.schedules.filter(schedule => schedule.task !== task);
-  }
+    // Return the next schedule to be executed
+    // Example output:
+    // {
+    //     schedule_id: '55adf9d94ecca5c17a5994db',
+    //     in_ms: 99998
+    // }
 
-  getSchedules() {
-    return this.schedules;
-  }
+    next() {
+    }  
 }
