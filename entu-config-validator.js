be51f0e5b2476fe7a2edc0075ee4a37ef1a00682
mitselek <mihkel.putrinsh@gmail.com>
2025-01-24 12:22:43 +0200

    entu configuration validation

---
+++ entu-config-validator.js
@@ -0,0 +1,63 @@
+class EntuConfigValidator {
+    constructor(configuration) {
+        this.configuration = configuration || {}
+        this.errors = []
+        this.warnings = []
+    }
+
+    validate() {
+        if (!this.configuration || Object.keys(this.configuration).length === 0) {
+            this.errors.push('Configuration is empty')
+            return this.getResult()
+        }
+
+        this.validateBasicStructure()
+        this.validateProperties()
+        this.validateRelations()
+        
+        return this.getResult()
+    }
+
+    validateBasicStructure() {
+        const required = ['_id', '_type', 'properties']
+        required.forEach(field => {
+            if (!this.configuration[field]) {
+                this.errors.push(`Missing required field: ${field}`)
+            }
+        })
+
+        if (this.configuration._type?.string !== 'sw_screen_group') {
+            this.errors.push('Invalid entity type: Must be sw_screen_group')
+        }
+    }
+
+    validateProperties() {
+        const properties = this.configuration.properties || {}
+        const required = ['name']
+
+        required.forEach(prop => {
+            if (!properties[prop] || !properties[prop].length) {
+                this.errors.push(`Missing required property: ${prop}`)
+            }
+        })
+    }
+
+    validateRelations() {
+        const relations = this.configuration._relationships || {}
+        const required = ['screens', 'configuration']
+
+        required.forEach(rel => {
+            if (!relations[rel] || !relations[rel].length) {
+                this.errors.push(`Missing required relation: ${rel}`)
+            }
+        })
+    }
+
+    getResult() {
+        return {
+            isValid: this.errors.length === 0,
+            errors: this.errors,
+            warnings: this.warnings
+        }
+    }
+}
