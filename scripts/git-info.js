ae4fbf78ee6653d4025a82f823691895d64bf893
mitselek <mihkel.putrinsh@gmail.com>
2025-05-19 14:05:50 +0300

    refactor: reorganize cron.js file structure and update import paths

--- scripts/git-info.js
+++
@@ -1,8 +0,0 @@
-
-// This file is auto-generated during build
-window.gitInfo = {
-  branch: 'local-development',
-  commit: 'dev',
-  buildTime: '2025-03-08T22:38:27.292Z',
-  deployUrl: 'http://localhost'
-}
