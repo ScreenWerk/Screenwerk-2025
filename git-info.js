1d64463f1fdb9a74d1d2c4e9d18d30afc3d5db23
mitselek <mihkel.putrinsh@gmail.com>
2025-05-19 13:19:42 +0300

    feat: update git-info.js to reset branch and commit information for production deployment

--- git-info.js
+++ git-info.js
@@ -1,8 +1,8 @@
 
 // This file is auto-generated during build
 window.gitInfo = {
-  branch: 'local-development',
-  commit: 'dev',
-  buildTime: '2025-05-18T17:41:33.690Z',
-  deployUrl: 'http://localhost'
+  branch: 'N/A',
+  commit: 'live',
+  buildTime: 'N/A',
+  deployUrl: 'N/A'
 }
